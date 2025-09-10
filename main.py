7d07805718ead0c397bec044e7fb86cc31fbb481
caonidaye698 <2545406514@qq.com>
2025-09-10 18:25:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ ENDTIME = "22:01:00"
 START_TIME = "22:00:00"
 
 ENABLE_SLIDER = True
-MAX_ATTEMPT = 2  # 减少到2次尝试，快速失败
+MAX_ATTEMPT = 1  # 减少到2次尝试，快速失败
 RESERVE_NEXT_DAY = True
 MAX_LOOP_ATTEMPTS = 2  # 减少循环次数
 MAX_WORKERS = 1  # 并发线程数
